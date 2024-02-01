7acb03c4bcab1e2a978e1d0505c1e766c7995c50
ABHISHEK KUMAR <abhishekbxr203@gmail.com>
2024-02-01 21:11:15 +0530

    portfolio

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GrTjlC83LXTktWJIG/nGZ2EgANPULduZA3b7uJ7rw6w=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"aq+inEHJrm8EeRw2zgWoA//yc6MNpVNkP51P7h+aHnk=\"\n}"
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,84 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Abhishek Kumar',
-  description: 'Web Developer | Software Engineer',
-}
+  metadataBase: new URL("https://abhishekbxr.website"),
+
+  title: {
+    default: "Abhishek Kumar - Experienced Software Engineer and Web Developer abhishekbxr",
+  },
+
+  description:
+    "Abhishek Kumar - Experienced Software Engineer and Web Developer | Transforming Ideas into Seamless Digital Solutions. Expertise in Full-Stack Development, UI/UX Design, and Problem-Solving. Available for Freelance Projects.| abhishekbxr",
+
+  generator: "Abhishek Kumar",
+  applicationName: "Abhishek Portfolio Website",
+  keywords: [
+    "Software Developer",
+    "Web Developer",
+    "Full-Stack Developer",
+    "UI/UX Designer",
+    "Programming Expert",
+    "Problem-Solver",
+    "Innovative Solutions",
+    "Code Optimization",
+    "Responsive Web Design",
+    "Front-End Development",
+    "Back-End Development",
+    "JavaScript",
+    "HTML5",
+    "CSS3",
+    "React.js",
+    "Node.js",
+    "PHP",
+    "Database Management",
+    "Freelance Developer",
+    "Digital Solutions Architect",
+  ],
+
+  authors: [{ name: "Abhishek kumar", url: "https://abhishekbxr.website" }],
+  creator: "Abhishek Kumar",
+  publisher: "Abhishek Kumar",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  alternates: {
+    canonical: "/",
+  },
+
+  openGraph: {
+    title: "Abhishek Kumar - Experienced Software Engineer and Web Developer",
+    description:
+      "Abhishek Kumar - Software and Web Developer with a passion for crafting innovative digital solutions. Specializing in Full-Stack Development and UI/UX Design. Open to exciting freelance opportunities. Let's build something amazing together!",
+
+    url: "https://abhishekbxr.com",
+    siteName: "Abhishek Portfolio",
+
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    title: "Abhishek Kumar - Experienced Software Engineer and Web Developer",
+    description:
+      "Abhishek Kumar - Software and Web Developer with a passion for crafting innovative digital solutions. Specializing in Full-Stack Development and UI/UX Design. Open to exciting freelance opportunities. Let's build something amazing together!",
+
+    handle: "@abhishekbxr",
+    site: "Abhishek Kumar",
+    card: "summary_large_image",
+  },
+  robots: {
+    content: "index, follow",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }

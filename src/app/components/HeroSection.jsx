941ae508de80ac68960525327064196c3699e730
ABHISHEK KUMAR <abhishekbxr203@gmail.com>
2024-04-01 23:04:51 +0530

    resume update

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -52,7 +52,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1sC2pUXp4D9HYNtBeMCT2CNW0FclMiCDo/view?usp=sharing"
+              href="https://drive.google.com/file/d/19gaLFIQc61VKMAevSvHc32KkV1asy4m6/view?usp=sharing"
               target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3 mr-3"
             >
